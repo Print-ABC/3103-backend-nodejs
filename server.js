2a6da3063b6cf1f904ee315e13488d2fa0732653
TanSK <tansk@Tans-MacBook-Pro.local>
2020-04-03 11:48:48 +0800

    Update port number

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ const http = require('http');
 const app = require('./app');
 const config = require('./config/config');
 
-//const port = process.env.PORT || parseInt(config.port);
-const port = 3000;
+const port = process.env.PORT || parseInt(config.port);
 
 const server = http.createServer(app);
 
